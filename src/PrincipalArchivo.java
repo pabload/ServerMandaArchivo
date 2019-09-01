0ff639476f4c3c83047d4a9cc19a7ca719f08452
Juan Pablo Armenta <juanpabload98@gmail.com>
2019-08-31 23:31:33 -0500

    ya manda archivo

--- src/PrincipalArchivo.java
+++ src/PrincipalArchivo.java
@@ -23,39 +23,38 @@ public class PrincipalArchivo {
     public static void main(String[] args) throws IOException {
         ServerSocket sockeServer = new ServerSocket(2500);
         Socket socket = sockeServer.accept();
-        DataOutputStream out = null;
         DataInputStream in = null;
         FileInputStream fis = null;
         BufferedInputStream bis = null;
         OutputStream os = null;
         BufferedReader lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter escritor= new PrintWriter(socket.getOutputStream(),true);
         String entrada;
         Scanner scanner =new  Scanner(System.in);
         String salida;
-        PrintWriter escritor= new PrintWriter(socket.getOutputStream(),true);
         do {
             entrada = lector.readLine();
             String cadena;
             System.out.println(entrada);
+            int longitud = (int) new File("C:\\Users\\G5\\Desktop\\mandaservidor\\"+entrada).length();
+            escritor.println(longitud);
             if (entrada.equalsIgnoreCase("fin")) {
                 System.out.println("me voy");
                 socket.close();
                 sockeServer.close();
                 System.exit(0);
             }
-              
-                File myFile = new File(entrada);
+                File myFile = new File("C:\\Users\\G5\\Desktop\\mandaservidor\\"+entrada);
                 byte[] mybytearray = new byte[(int) myFile.length()];
                 fis = new FileInputStream(myFile);
                 bis = new BufferedInputStream(fis);
                 bis.read(mybytearray, 0, mybytearray.length);
                 os = socket.getOutputStream();
-                System.out.println("Sending " + entrada + "(" + mybytearray.length + " bytes)");
+                System.out.println("enviando " + entrada + "(" + mybytearray.length + " bytes)");
+                //escritor.print(longitud);
                 os.write(mybytearray, 0, mybytearray.length);
                 os.flush();
-                System.out.println("Done.");
-
-            
+                System.out.println("ya."+"222222222222222222");
            
         } while (!entrada.equalsIgnoreCase("fin"));
     
