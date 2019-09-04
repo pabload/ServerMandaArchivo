e5c84f5e564efc8a4640912d34828b52f8e963a8
Juan Pablo Armenta <juanpabload98@gmail.com>
2019-09-03 23:18:40 -0500

    se puso mas validaciones

--- src/PrincipalArchivo.java
+++ src/PrincipalArchivo.java
@@ -16,50 +16,66 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-
 public class PrincipalArchivo {
 
-    
     public static void main(String[] args) throws IOException {
-        ServerSocket sockeServer = new ServerSocket(2500);
-        Socket socket = sockeServer.accept();
-        DataInputStream in = null;
-        FileInputStream fis = null;
-        BufferedInputStream bis = null;
-        OutputStream os = null;
-        BufferedReader lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter escritor= new PrintWriter(socket.getOutputStream(),true);
-        String entrada;
-        Scanner scanner =new  Scanner(System.in);
-        String salida;
-        do {
-            entrada = lector.readLine();
-            String cadena;
-            System.out.println(entrada);
-            int longitud = (int) new File("C:\\Users\\G5\\Desktop\\mandaservidor\\"+entrada).length();
-            escritor.println(longitud);
-            if (entrada.equalsIgnoreCase("fin")) {
-                System.out.println("me voy");
-                socket.close();
-                sockeServer.close();
-                System.exit(0);
+        while (true) {
+            System.out.println("ruta de tu carpeta servidor");
+            String ruta;
+            Scanner scanner = new Scanner(System.in);
+            ruta = scanner.nextLine();
+            File direcion = new File(ruta);
+            if (direcion.exists()) {
+                System.out.println("existe");
+                ServerSocket sockeServer = new ServerSocket(2500);
+                Socket socket = sockeServer.accept();
+                DataInputStream in = null;
+                FileInputStream fis = null;
+                BufferedInputStream bis = null;
+                OutputStream os = null;
+                BufferedReader lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter escritor = new PrintWriter(socket.getOutputStream(), true);
+                String entrada;
+                //Scanner scanner =new  Scanner(System.in);
+                String salida;
+
+                do {
+                    entrada = lector.readLine();
+                    File archivo = new File(direcion+"\\"+entrada);
+                    System.out.println(archivo);
+                    //escritor.println("xd");
+                    if (archivo.exists()) {
+                        String cadena;
+                        System.out.println(entrada);
+                        int longitud = (int) new File(direcion+"\\"+entrada).length();
+                        escritor.println(longitud);
+                        byte[] mybytearray = new byte[(int) archivo.length()];
+                        fis = new FileInputStream(archivo);
+                        bis = new BufferedInputStream(fis);
+                        bis.read(mybytearray, 0, mybytearray.length);
+                        os = socket.getOutputStream();
+                        System.out.println("enviando " + entrada + "(" + mybytearray.length + " bytes)");
+                        //escritor.print(longitud);
+                        os.write(mybytearray, 0, mybytearray.length);
+                        os.flush();
+                        System.out.println("ya.");
+                    }else{
+                        escritor.println("non");
+                    }
+                    if (entrada.equalsIgnoreCase("fin")) {
+                        System.out.println("me voy");
+                        escritor.println("fin");
+                        socket.close();
+                        sockeServer.close();
+                        System.exit(0);
+                    }
+
+                } while (!entrada.equalsIgnoreCase("fin"));
+            } else {
+                System.out.println("no existe");
             }
-                File myFile = new File("C:\\Users\\G5\\Desktop\\mandaservidor\\"+entrada);
-                byte[] mybytearray = new byte[(int) myFile.length()];
-                fis = new FileInputStream(myFile);
-                bis = new BufferedInputStream(fis);
-                bis.read(mybytearray, 0, mybytearray.length);
-                os = socket.getOutputStream();
-                System.out.println("enviando " + entrada + "(" + mybytearray.length + " bytes)");
-                //escritor.print(longitud);
-                os.write(mybytearray, 0, mybytearray.length);
-                os.flush();
-                System.out.println("ya."+"222222222222222222");
-           
-        } while (!entrada.equalsIgnoreCase("fin"));
-    
-   }
+        }
+
+    }
 }
-   
-        
 
