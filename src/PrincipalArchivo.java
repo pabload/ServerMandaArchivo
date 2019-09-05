ac604ba42f4cad1324170dbadc2afe6dc52e098c
Juan Pablo Armenta <juanpabload98@gmail.com>
2019-09-05 16:56:23 -0600

    Falta tries

--- src/PrincipalArchivo.java
+++ src/PrincipalArchivo.java
@@ -19,63 +19,58 @@ import java.util.Scanner;
 public class PrincipalArchivo {
 
     public static void main(String[] args) throws IOException {
-        while (true) {
-            System.out.println("ruta de tu carpeta servidor");
-            String ruta;
-            Scanner scanner = new Scanner(System.in);
-            ruta = scanner.nextLine();
-            File direcion = new File(ruta);
-            if (direcion.exists()) {
-                System.out.println("existe");
-                ServerSocket sockeServer = new ServerSocket(2500);
-                Socket socket = sockeServer.accept();
-                DataInputStream in = null;
-                FileInputStream fis = null;
-                BufferedInputStream bis = null;
-                OutputStream os = null;
-                BufferedReader lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                PrintWriter escritor = new PrintWriter(socket.getOutputStream(), true);
-                String entrada;
-                //Scanner scanner =new  Scanner(System.in);
-                String salida;
+        ServerSocket sockeServer = new ServerSocket(2500);
+        Socket socket = sockeServer.accept();
+        DataInputStream in = null;
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        OutputStream os = null;
+        BufferedReader lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter escritor = new PrintWriter(socket.getOutputStream(), true);
+        String entrada;
+        String salida;
+        String ruta = lector.readLine();
+        System.out.println("ruta de tu carpeta servidor");
+        //String lector.readLine()
+        File direcion = new File(ruta);
+        if (direcion.exists()) {
+            System.out.println("existe");
 
-                do {
-                    entrada = lector.readLine();
-                    File archivo = new File(direcion+"\\"+entrada);
-                    System.out.println(archivo);
-                    //escritor.println("xd");
-                    if (archivo.exists()) {
-                        String cadena;
-                        System.out.println(entrada);
-                        int longitud = (int) new File(direcion+"\\"+entrada).length();
-                        escritor.println(longitud);
-                        byte[] mybytearray = new byte[(int) archivo.length()];
-                        fis = new FileInputStream(archivo);
-                        bis = new BufferedInputStream(fis);
-                        bis.read(mybytearray, 0, mybytearray.length);
-                        os = socket.getOutputStream();
-                        System.out.println("enviando " + entrada + "(" + mybytearray.length + " bytes)");
-                        //escritor.print(longitud);
-                        os.write(mybytearray, 0, mybytearray.length);
-                        os.flush();
-                        System.out.println("ya.");
-                    }else{
-                        escritor.println("non");
-                    }
-                    if (entrada.equalsIgnoreCase("fin")) {
-                        System.out.println("me voy");
-                        escritor.println("fin");
-                        socket.close();
-                        sockeServer.close();
-                        System.exit(0);
-                    }
+            do {
+                entrada = lector.readLine();
+                File archivo = new File(entrada);
+                System.out.println(archivo);
+                //escritor.println("xd");
+                if (archivo.exists()) {
+                    String cadena;
+                    System.out.println(entrada);
+                    int longitud = (int) new File(entrada).length();
+                    escritor.println(longitud);
+                    byte[] mybytearray = new byte[(int) archivo.length()];
+                    fis = new FileInputStream(archivo);
+                    bis = new BufferedInputStream(fis);
+                    bis.read(mybytearray, 0, mybytearray.length);
+                    os = socket.getOutputStream();
+                    System.out.println("enviando " + entrada + "(" + mybytearray.length + " bytes)");
+                    //escritor.print(longitud);
+                    os.write(mybytearray, 0, mybytearray.length);
+                    os.flush();
+                    System.out.println("ya.");
+                } else {
+                    escritor.println("non");
+                }
+                if (entrada.equalsIgnoreCase("fin")) {
+                    System.out.println("me voy");
+                    escritor.println("fin");
+                    socket.close();
+                    sockeServer.close();
+                    System.exit(0);
+                }
 
-                } while (!entrada.equalsIgnoreCase("fin"));
-            } else {
-                System.out.println("no existe");
-            }
+            } while (!entrada.equalsIgnoreCase("fin"));
+        } else {
+            System.out.println("no existe");
         }
-
     }
-}
 
+}
